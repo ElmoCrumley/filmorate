2a4c2440fa3a2530ac4e657e8823a2bf85f5ad3a
ElmoCrumley <MW826124@gmail.com>
2025-08-08 11:48:01 +0300

    Добавить аннотации @NotNull, @NotBlank, @Positive.

--- src/main/java/ru/yandex/practicum/model/Film.java
+++ src/main/java/ru/yandex/practicum/model/Film.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,8 +10,12 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private Long id;
+    @NotNull
+    @NotBlank
     private String name;
     private String description;
+    @NotNull
     private LocalDate releaseDate;
+    @Positive
     private int duration;
 }
