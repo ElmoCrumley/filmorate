91bdfe2140c8c93864bbc3fc7dfa7936af10aa1d
ElmoCrumley <MW826124@gmail.com>
2025-09-24 10:51:10 +0300

    Добавить логирование выброса ошибок

--- src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,30 +9,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.exception.NotFoundException;
 import ru.yandex.practicum.exception.ValidationException;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationArgumentsException(Exception e) {
+        log.error("* Exception *, " + "Method argument isn't valid");
         return new ErrorResponse("* Exception *" + "Method argument isn't valid");
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(ValidationException e) {
+        log.error("* ValidationException *, {}", e.getMessage());
         return new ErrorResponse("* Exception *" + e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException e) {
+        log.error("* NotFoundException *, {}", e.getMessage());
         return new ErrorResponse("* Exception *" + e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(Throwable e) {
+        log.error("* Throwable *, {}", e.getMessage());
         return new ErrorResponse("* Exception *" + "Произошла непредвиденная ошибка.");
     }
 }
