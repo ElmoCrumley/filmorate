60155701f2effad44f73ac7819f1b070f5f88f0e
ElmoCrumley <MW826124@gmail.com>
2026-02-05 11:58:45 +0300

    Добавить в маппер фильмов возможность собирать объект фильма полностью, с учётом жанров и рейтинга. Добавить возможность вернуть список фильмов.

--- src/main/java/ru/yandex/practicum/storage/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/storage/mappers/FilmRowMapper.java
@@ -1,24 +1,63 @@
 package ru.yandex.practicum.storage.mappers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.model.Film;
+import ru.yandex.practicum.model.Genre;
+import ru.yandex.practicum.model.MotionPictureAA;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 @Component
+@Slf4j
 public class FilmRowMapper implements RowMapper<Film> {
+    private final Map<Long, Film> filmMap = new HashMap<>();
+
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Film film = new Film();
+        log.info("------------- * Start * FilmRowMapper * mapRow() -------------");
 
-        film.setId(rs.getLong("id"));
-        film.setTitle(rs.getString("title"));
-        film.setDescription(rs.getString("description"));
-        film.setReleaseDate(rs.getDate("releaseDate").toLocalDate());
-        film.setDuration(rs.getInt("duration"));
-        film.setMotionPictureAssociation(rs.getString("motionPictureAssociation"));
+        long filmId = rs.getLong("id");
+
+        Film film = filmMap.get(filmId);
+
+        if (film == null) {
+            film = new Film();
+            film.setId(filmId);
+            film.setName(rs.getString("name"));
+            film.setDescription(rs.getString("description"));
+            film.setReleaseDate(rs.getTimestamp("releaseDate").toLocalDateTime().toLocalDate());
+            film.setDuration(rs.getInt("duration"));
+            film.setGenres(new HashSet<>());
+            filmMap.put(filmId, film);
+        }
+
+        Integer mpaId = rs.getObject("mpa_id", Integer.class);
+
+        if (mpaId != null && film.getMpa() == null) {
+            MotionPictureAA motionPictureAA = new MotionPictureAA();
+            motionPictureAA.setId(mpaId);
+            motionPictureAA.setName(rs.getString("mpa_name"));
+            film.setMpa(motionPictureAA);
+        }
+
+        Integer genreId = rs.getObject("genre_id", Integer.class);
+
+        if (genreId != null) {
+            Genre genre = new Genre();
+            genre.setId(genreId);
+            genre.setName(rs.getString("genre_name"));
+            film.getGenres().add(genre);  // Добавляем только уникальные жанры
+        }
+
+        log.info("------------- * return * ------------- {} \n", film);
         return film;
+    }
+
+    public Collection<Film> getFilms() {
+        return filmMap.values();
     }
 }
