da9eb2d5d1e065880e50dbefc8e6313f07bcf6c7
ElmoCrumley <MW826124@gmail.com>
2026-02-05 12:00:15 +0300

    Добавить в маппер пользователей возможность собрать пользователя с учётом списка друзей и вернуть список пользователей с информацией о друзьях.

--- src/main/java/ru/yandex/practicum/storage/mappers/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/storage/mappers/UserRowMapper.java
@@ -5,18 +5,39 @@ import ru.yandex.practicum.model.User;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class UserRowMapper implements RowMapper<User> {
+    private final Map<Long, User> userMap = new HashMap<>();
+
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        User user = new User();
+        long userId = rs.getLong("id");
 
-        user.setId(rs.getLong("id"));
-        user.setEmail(rs.getString("email"));
-        user.setLogin(rs.getString("login"));
-        user.setName(rs.getString("name"));
-        user.setBirthday(rs.getDate("birthday").toLocalDate());
+        User user = userMap.get(userId);
+
+        if (user == null) {
+            user = new User();
+            user.setId(rs.getLong("id"));
+            user.setEmail(rs.getString("email"));
+            user.setLogin(rs.getString("login"));
+            user.setName(rs.getString("name"));
+            user.setBirthday(rs.getDate("birthday").toLocalDate());
+        }
+
+        Long friendId = rs.getObject("confirmed_friend_id", Long.class);
+
+        if (friendId != null) {
+            user.getFriendshipConfirmed().add(friendId);
+        }
+
         return user;
+    }
+
+    public Collection<User> getUsers() {
+        return userMap.values();
     }
 }
