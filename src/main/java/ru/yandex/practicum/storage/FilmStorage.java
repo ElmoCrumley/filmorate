fe976af09b8fbfcffedf105880d5a9e14023e647
ElmoCrumley <MW826124@gmail.com>
2026-02-05 11:54:12 +0300

    Добавить методы findAllGenres, findGenreById, findAllMPAs, findMPAById. Обернуть объекты в Optional в методах findById, update, delete класса FilmStorage и в методах findById, update класса UserStorage.

--- src/main/java/ru/yandex/practicum/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/storage/FilmStorage.java
@@ -1,21 +1,24 @@
 package ru.yandex.practicum.storage;
 
 import ru.yandex.practicum.model.Film;
+import ru.yandex.practicum.model.Genre;
+import ru.yandex.practicum.model.MotionPictureAA;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface FilmStorage {
     // films CRUDs
     public Collection<Film> findAll();
 
-    public Film findById(Long id);
+    public Optional<Film> findById(Long id);
 
     public Film include(Film film);
 
-    public Film update(Film film);
+    public Optional<Film> update(Film film);
 
-    public Film delete(Film film);
+    public Optional<Film> delete(Film film);
 
     // CRUDs of likes
     public void addLike(Long filmId, Long userId);
@@ -25,4 +28,13 @@ public interface FilmStorage {
     // read populars
     public List<Film> getMostPopular(long count);
 
+    // genres
+    Collection<Genre> findAllGenres();
+
+    Optional<Genre> findGenreById(Integer genreId);
+
+    // mpa's
+    Collection<MotionPictureAA> findAllMPAs();
+
+    Optional<MotionPictureAA> findMPAById(Integer mpaId);
 }
--- src/main/java/ru/yandex/practicum/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/storage/UserStorage.java
@@ -1,24 +1,23 @@
 package ru.yandex.practicum.storage;
 
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.model.User;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface UserStorage {
     // users CRUDs
     public Collection<User> findAll();
 
-    public User findById(Long userId);
+    public Optional<User> findById(Long userId);
 
-    public User create(@Valid @RequestBody User user);
+    public User create(User user);
 
-    public User update(@Valid @RequestBody User user);
+    public Optional<User> update(User user);
 
-    public void delete(@Valid @RequestBody User user);
+    public void delete(User user);
 
     // CRUDs of friendship
     public List<User> getFriends(Long id);
